064f0efc71a4a7617f79d8379f42853ec1ccdd77
Steffany27 <steffanysancosta@uni9.edu.br>
2025-11-17 23:10:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ class Program
         FolhaPagamento folha = new FolhaPagamento(salarioBruto);
 
         double inss = folha.CalcularINSS();
+        double salarioBase = salarioBruto - inss;
         double irrf = folha.CalcularIRRF();
 
-        Console.WriteLine($"
-Desconto INSS: {inss:F2}");
-        Console.WriteLine($"Desconto IRRF: {irrf:F2}");
+        Console.WriteLine($"Salário bruto: R$ {salarioBruto:F2}");
+        Console.WriteLine($"Desconto INSS: R$ {inss:F2}");
+        Console.WriteLine($"Salário base: R$ {salarioBase:F2}");
+        Console.WriteLine($"Desconto IRRF: R$ {irrf:F2}");
     }
 }
